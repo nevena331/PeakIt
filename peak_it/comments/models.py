65cca2f06b1fb7b840574ddb3319c135f869b83e
nevena331 <nevena.dimitrova2021@gmail.com>
2025-06-06 11:54:25 +0300

    added comment model in the app

--- peak_it/comments/models.py
+++ peak_it/comments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Comment(models.Model):
+    body = models.TextField(_("comment body"), blank=False)
+    post = models.ForeignKey("adventures.Adventure", verbose_name=_("post"), on_delete=models.CASCADE)
+    writer = models.ForeignKey("users.CustomUser", verbose_name=_("writer"), on_delete=models.CASCADE)
+    written_on = models.DateTimeField(_("date"), auto_now=False, auto_now_add=True)
+
+
